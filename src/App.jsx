6a04db9bef7176a971818b788d8a826f51c60e46
minho0391 <minh05@naver.com>
2026-07-03 16:42:29 +0900

    feat: implement view and modify functionality in BoardList and Write components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router";
-import BoardList from "./components/BoardList";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Routes, Route, useNavigate } from "react-router";
+import BoardList from "./components/BoardList";
 import Write from "./components/Write";
+import View from "./components/View";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [boardId, setBoardId] = useState(0);
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const navigate = useNavigate();
 
+  const handleModify = _id => {
+    setBoardId(_id);
+    setIsModifyMode(true);
+    navigate("/write");
+  };
+  const handleCancel = () => {
+    setBoardId(0);
+    setIsModifyMode(false);
+  };
   return (
     <div className="container">
       <h1>React BBS</h1>
       <Routes>
         <Route path="/" element={<BoardList />} />
-        <Route path="/write" element={<Write />} />
-        {/* <Route path="/view.id" element={<View />} /> */}
+        <Route
+          path="/write"
+          element={
+            <Write isModifyMode={isModifyMode} boardId={boardId} handleCancel={handleCancel} />
+          }
+        />
+        <Route path="/view/:id" element={<View handleModify={handleModify} />} />
       </Routes>
     </div>
   );
